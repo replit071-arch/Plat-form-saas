c773a35b5525f10bc286b46f99d354f31bf35b81
replit071-arch <replit071@gmail.com>
2026-02-15 20:53:09 +0530

    Update App.jsx
    Change Due To Deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,33 +9,15 @@ import Register from './pages/auth/Register';
 // Root Admin
 import RootDashboard from './pages/root/Dashboard';
 import AdminManagement from './pages/root/AdminManagement';
-import PlanManagement from './pages/root/PlanManagement';
-import RootTickets from './pages/root/Tickets';
 
 // Admin
 import AdminDashboard from './pages/admin/Dashboard';
 import ChallengeList from './pages/admin/challenges/ChallengeList';
 import CreateChallenge from './pages/admin/challenges/CreateChallenge';
-import EditChallenge from './pages/admin/challenges/EditChallenge';
-import UserManagement from './pages/admin/UserManagement';
-import CouponManagement from './pages/admin/CouponManagement';
-import BrandingSettings from './pages/admin/BrandingSettings';
-import AdminTickets from './pages/admin/Tickets';
-import PayoutManagement from './pages/admin/PayoutManagement';
-
-// User
-import UserDashboard from './pages/user/Dashboard';
-import BrowseChallenges from './pages/user/BrowseChallenges';
-import MyChallenges from './pages/user/MyChallenges';
-import RequestPayout from './pages/user/RequestPayout';
-import UserTickets from './pages/user/Tickets';
-import MyCertificates from './pages/user/Certificates';
-import Referrals from './pages/user/Referrals';
 
 // Layout
 import RootLayout from './layouts/RootLayout';
 import AdminLayout from './layouts/AdminLayout';
-import UserLayout from './layouts/UserLayout';
 
 // Protected Route Component
 import ProtectedRoute from './components/ProtectedRoute';
@@ -66,8 +48,6 @@ function App() {
           }>
             <Route index element={<RootDashboard />} />
             <Route path="admins" element={<AdminManagement />} />
-            <Route path="plans" element={<PlanManagement />} />
-            <Route path="tickets" element={<RootTickets />} />
           </Route>
 
           {/* Admin Routes */}
@@ -79,27 +59,6 @@ function App() {
             <Route index element={<AdminDashboard />} />
             <Route path="challenges" element={<ChallengeList />} />
             <Route path="challenges/create" element={<CreateChallenge />} />
-            <Route path="challenges/edit/:id" element={<EditChallenge />} />
-            <Route path="users" element={<UserManagement />} />
-            <Route path="coupons" element={<CouponManagement />} />
-            <Route path="branding" element={<BrandingSettings />} />
-            <Route path="tickets" element={<AdminTickets />} />
-            <Route path="payouts" element={<PayoutManagement />} />
-          </Route>
-
-          {/* User Routes */}
-          <Route path="/user" element={
-            <ProtectedRoute role="user">
-              <UserLayout />
-            </ProtectedRoute>
-          }>
-            <Route index element={<UserDashboard />} />
-            <Route path="challenges" element={<BrowseChallenges />} />
-            <Route path="my-challenges" element={<MyChallenges />} />
-            <Route path="payout" element={<RequestPayout />} />
-            <Route path="tickets" element={<UserTickets />} />
-            <Route path="certificates" element={<MyCertificates />} />
-            <Route path="referrals" element={<Referrals />} />
           </Route>
 
           {/* Default redirect */}
@@ -111,3 +70,5 @@ function App() {
 }
 
 export default App;
+
+
